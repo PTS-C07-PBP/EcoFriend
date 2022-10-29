fe713262c5b5bf452d00dcc0d1b0a6f7dff03d23
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2022-10-30 01:05:47 +0700

    Completed more of the given criteria

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
+from .forms import ArticleForm
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    form = ArticleForm
+admin.site.register(Article, ArticleAdmin)
