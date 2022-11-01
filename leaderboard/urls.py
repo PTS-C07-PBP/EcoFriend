a20e24d902444ef90fb868270ed12c9233d93d1f
farelakasya <farel.rishad@ui.ac.id>
2022-11-01 21:10:20 +0700

    3

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -6,5 +6,6 @@ from leaderboard.views import show_leaderboard
 app_name = 'leaderboard'
 
 urlpatterns = [
-    path('', show_leaderboard , name='show_leaderboard'),
+    path('', show_leaderboard , name = 'show_leaderboard'),
+
 ]
