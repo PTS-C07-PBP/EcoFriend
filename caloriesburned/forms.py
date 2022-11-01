93e95091ab1413bd3bca751c9cbcd594771a27f6
valencius.apriady <v.apriady@gmail.com>
2022-11-01 21:03:29 +0700

    editing some fiture

--- caloriesburned/forms.py
+++ caloriesburned/forms.py
@@ -4,4 +4,4 @@ class userWeight(forms.Form):
     weight = forms.FloatField(label="Body weight in pounds")
 
 class addMotive(forms.Form):
-    motive = forms.CharField(label="Add new motive")
+    motive = forms.CharField(label="Add new motivation")
