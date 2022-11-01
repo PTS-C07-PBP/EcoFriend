d38ef20d3660420cdab0f9fd636429558625256e
kayleerudaina <kayleerudaina@gmail.com>
2022-11-01 22:03:04 +0700

    edit user fields

--- user/models.py
+++ user/models.py
@@ -4,9 +4,10 @@ from tracker.models import Footprint
 
 class User(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    is_admin = models.BooleanField(default=False)
     calories = models.FloatField(null=True, blank=True)
     carbon = models.FloatField(null=True, blank=True)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username','first_name','last_name']
+    REQUIRED_FIELDS = ['username','first_name','last_name','is_admin']
 
