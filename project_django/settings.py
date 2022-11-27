8276c51f23d35ede6c495ba4d54a657cc2a3e9e3
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2022-11-27 19:11:24 +0700

    Railway integration

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [f'ecofriend.up.railway.app']
+ALLOWED_HOSTS = ['ecofriend.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['ecofriend.up.railway.app']
 
 # Application definition
 
