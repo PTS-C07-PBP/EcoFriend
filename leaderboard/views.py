b54c070dd83b8346e44351224803633d0bf32d1e
farelakasya <farel.rishad@ui.ac.id>
2022-12-12 14:52:13 +0700

    authentication

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -13,32 +13,33 @@ from .forms import CommentForm
 
 # Create your views here.
 
-@login_required(login_url='/user/login/')
 def show_leaderboard(request):
+    if request.user.is_authenticated:
+        form = CommentForm()
+
+        data = []
+        User = get_user_model()
+        comment = Comment.objects.all()
+        user_list = User.objects.all()
+        for user1 in user_list:
+            tracker = Footprint.objects.filter(user=user1.pk)
+            sum_mileage = 0
+            sum_carbon = 0
+            for fp in tracker:
+                sum_mileage += fp.mileage
+                sum_carbon += fp.carbon
+            if sum_mileage:
+                data.append([user1, sum_carbon/sum_mileage, sum_mileage, sum_carbon])
+        data.sort(key=lambda x: x[1])
+        context = {
+            'list': data,
+            'form': form,
+            'comments': comment
+        }
+        return render(request, 'leaderboard.html', context)
     form = CommentForm()
+    return render(request, 'denied.html', {'form': form})
 
-    data = []
-    User = get_user_model()
-    comment = Comment.objects.all()
-    user_list = User.objects.all()
-    for user1 in user_list:
-        tracker = Footprint.objects.filter(user=user1.pk)
-        sum_mileage = 0
-        sum_carbon = 0
-        for fp in tracker:
-            sum_mileage += fp.mileage
-            sum_carbon += fp.carbon
-        if sum_mileage:
-            data.append([user1, sum_carbon/sum_mileage, sum_mileage, sum_carbon])
-    data.sort(key=lambda x: x[1])
-    context = {
-        'list': data,
-        'form': form,
-        'comments': comment
-    }
-    return render(request, 'leaderboard.html', context)
-
-@login_required(login_url='/user/login/')
 def show_json(request):
     form = CommentForm()
 
